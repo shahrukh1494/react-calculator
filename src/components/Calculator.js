5d60a7be2349d636788a485308e9d2514422402d
unknown <shahrukh1494@gmail.com>
2021-03-31 16:46:29 +0530

    first commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+
+const Calculator = () => {
+  const [data, setData] = useState("");
+  let length = data.length;
+
+  const handleClick = (e) => {
+    setData(data + e.target.name);
+  };
+
+  const addZero = (e) => {
+    //check for zeroes before number
+    if (
+      data !== "" &&
+      data.charAt(length - 1) !== "+" &&
+      data.charAt(length - 1) !== "-" &&
+      data.charAt(length - 1) !== "/" &&
+      data.charAt(length - 1) !== "*"
+    ) {
+      setData(data + e.target.name);
+    }
+  };
+
+  const addDecimal = (e) => {
+    //add only one decimal per number
+    if (
+      data.indexOf(".") === -1 ||
+      data.slice(data.lastIndexOf("+") + 1, length + 1).indexOf(".") === -1 ||
+      data.slice(data.lastIndexOf("-") + 1, length + 1).indexOf(".") === -1 ||
+      data.slice(data.lastIndexOf("/") + 1, length + 1).indexOf(".") === -1 ||
+      data.slice(data.lastIndexOf("*") + 1, length + 1).indexOf(".") === -1
+    ) {
+      setData(data + e.target.name);
+    }
+  };
+
+  const calculate = () => {
+    try {
+      setData(eval(data).toString());
+    } catch (err) {
+      setData("ERROR! WRONG INPUT");
+    }
+  };
+
+  //CE button function to clear input
+  const clear = () => {
+    setData("");
+  };
+
+  //delete button function for backspace
+  const del = () => {
+    setData(data.slice(0, length - 1));
+  };
+
+  return (
+    <div>
+      <div className="data">{data}</div>
+      <div className="keypad">
+        <button className="clear" onClick={clear}>
+          CE
+        </button>
+        <button className="delete" onClick={del}>
+          DEL
+        </button>
+        <button className="operator" name="/" onClick={handleClick}>
+          &divide;
+        </button>
+        <button name="7" onClick={handleClick}>
+          7
+        </button>
+        <button name="8" onClick={handleClick}>
+          8
+        </button>
+        <button name="9" onClick={handleClick}>
+          9
+        </button>
+        <button name="*" className="operator" onClick={handleClick}>
+          &times;
+        </button>
+        <button name="4" onClick={handleClick}>
+          4
+        </button>
+        <button name="5" onClick={handleClick}>
+          5
+        </button>
+        <button name="6" onClick={handleClick}>
+          6
+        </button>
+        <button name="-" className="operator" onClick={handleClick}>
+          -
+        </button>
+        <button name="1" onClick={handleClick}>
+          1
+        </button>
+        <button name="2" onClick={handleClick}>
+          2
+        </button>
+        <button name="3" onClick={handleClick}>
+          3
+        </button>
+        <button name="+" className="operator" onClick={handleClick}>
+          +
+        </button>
+        <button name="." onClick={addDecimal}>
+          .
+        </button>
+        <button name="0" onClick={addZero}>
+          0
+        </button>
+        <button className="result" onClick={calculate}>
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
